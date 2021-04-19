de2bb089290fb1e200f7ba1bc2f6983a582229f2
ePietrocola <enricopietrocola@gmail.com>
2021-04-19 19:50:59 +0200

    Fixed reconnect, broadcast and join

--- javascript/maxSocketIOClient.js
+++ javascript/maxSocketIOClient.js
@@ -4,8 +4,8 @@ const Max = require('max-api');
 
 const io = require("socket.io-client");
 
-let ioClient = io.connect("http://116.203.114.204:5000");
-//let ioClient = io.connect("http://127.0.0.1:5000");
+//let ioClient = io.connect("http://116.203.114.204:5000");
+let ioClient = io.connect("http://127.0.0.1:5000");
 
 let roomName;
 // This will be printed directly to the Max console
@@ -19,23 +19,43 @@ Max.addHandler("roomName", (msg)=> {
 })
 
 Max.addHandler("address", (msg)=> {
-    ioClient.disconnect().then(() => {
-        ioClient = io.connect(msg)
-    })
+    ioClient.disconnect()
+    ioClient.connect(msg)
 })
 
 Max.addHandler("send", (msg) => {
     //Max.post(msg);
-    ioClient.emit("datachannel", roomName,  msg);
+    ioClient.emit("datachannel", roomName, JSON.stringify(msg));
 });
 
 ioClient.on('datachannel', (msg)=> {
+    if (msg !== undefined && msg !== null) {
+        if (isJson(msg)) {
+            msg = JSON.parse(msg)
+        } else {
+        }
+    }
     console.log("DEBUG received " + msg)
     Max.outlet(msg)
 })
 
 ioClient.on("disconnect", (msg)=> {
+    if (msg !== undefined && msg !== null) {
+        if (isJson(msg)) {
+            msg = JSON.parse(msg)
+        } else {
+
+        }
+    }
     Max.post("disconnected")
-    console.log("disconnected")
     Max.outlet("disconnected")
 })
+
+function isJson(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
